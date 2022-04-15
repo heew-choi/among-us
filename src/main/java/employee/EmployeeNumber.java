7cbf14f6f7cde9cc79a49a5fa4d6ba69068eb2d8
fanpa <fanpa9@gmail.com>
2022-04-15 14:29:19 +0900

    [Mod] EmployeeNumber 누락된 getter/setter 추가

--- src/main/java/employee/EmployeeNumber.java
+++ src/main/java/employee/EmployeeNumber.java
@@ -3,10 +3,25 @@ package employee;
 import exceptions.ImproperlyConfigured;
 
 public class EmployeeNumber extends EmployeeField {
-    String fullNumber;
-    String shortNumber;
+    private String fullNumber;
+    private String shortNumber;
 
     public EmployeeNumber(String shortNumber) throws ImproperlyConfigured {
         super(shortNumber);
+    public String getFullNumber() {
+        return fullNumber;
+    }
+
+    public void setFullNumber(String fullNumber) {
+        this.fullNumber = fullNumber;
+    }
+
+    public String getShortNumber() {
+        return shortNumber;
+    }
+
+    public void setShortNumber(String shortNumber) {
+        this.shortNumber = shortNumber;
+    }
     }
 }
