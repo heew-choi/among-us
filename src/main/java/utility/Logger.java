9fbbee4d7b4249fd314733e09e275f4d9c7d4707
fanpa <fanpa9@gmail.com>
2022-04-18 11:18:24 +0900

    [Add] Log를 쌓아두었다가 한번에 출력하는 method 추가

--- src/main/java/utility/Logger.java
+++ src/main/java/utility/Logger.java
@@ -4,10 +4,12 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Logger {
     private static File file = null;
     private static final String OUTPUT_FILE_PATH = "output.txt";
+    private static ArrayList<String> printBuffer = new ArrayList<>();
 
     private Logger() {
         throw new IllegalStateException("Utility class");
@@ -15,6 +17,33 @@ public class Logger {
 
     public static void logConsole(String message) {
         System.out.println(message);
+    }
+
+    public static void appendLog(String message) {
+        printBuffer.add(message);
+    }
+
+    public static void flushLogToFile() {
+        if (!getOrCreateFile()) {
+            return;
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, false))) {
+            for (String message: printBuffer) {
+                writer.write(message);
+                writer.newLine();
+            }
+            writer.flush();
+            printBuffer.clear();
+        } catch (IOException ex) {
+            logConsole("IO Exception in logger");
+            logConsole(ex.toString());
+        }
+    }
+
+    public static void flushLogToConsole() {
+        for (String message: printBuffer) {
+            logConsole(message);
+        }
     }
 
     public static void logFile(String message) {
