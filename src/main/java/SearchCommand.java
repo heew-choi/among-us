584c4ff297d623752194e9a7a3eaddaa924f27c3
BronnzSon <79143217+BronnzSon@users.noreply.github.com>
2022-04-18 16:25:57 +0900

    [Add] SearchCommand 코드 구현

--- src/main/java/SearchCommand.java
+++ src/main/java/SearchCommand.java
@@ -1,4 +1,10 @@
+import employee.Employee;
+
+import java.util.ArrayList;
+
 public class SearchCommand extends Command {
+    public ArrayList<Employee> testResult;
+
     @Override
     public String getCommandType() {
         return "SCH";
@@ -6,6 +12,15 @@ public class SearchCommand extends Command {
 
     @Override
     public void run() {
+        try {
+//            print(database.select(option.compareOption));
 
+            // For unit test
+            testResult = database.select(option.compareOption);
+            print(testResult);
+        }
+        catch (Exception e) {
+            throw e;
+        }
     }
 }
