e010a64f65f1c941db1039499d7867587799a5a1
최희원 <kc330mrtt@naver.com>
2022-04-18 11:31:01 +0900

    [ADD] 항목별 검색 테스트케이스 추가

--- src/test/java/database/SelectTest.java
+++ src/test/java/database/SelectTest.java
@@ -29,26 +29,86 @@ public class SelectTest {
     }
 
     @Test
-    @DisplayName("이름(성) 조건 검색 테스트")
-    void lastNameConditionSelectTest() throws ImproperlyConfigured {
-        assertEquals(1, db.select(new LastNameCompareOption("CHOI")).size(), "Select 결과 확인");
+    @DisplayName("사번 조건 검색 테스트")
+    void employeeNumberConditionSelectTest() throws ImproperlyConfigured {
+        assertEquals(1, db.select(new EmployeeNumberCompareOption("99887766")).size(), "검색 결과 존재하는 케이스");
+        assertEquals(0, db.select(new EmployeeNumberCompareOption("13004450")).size(), "검색 결과 존재하지 않는 케이스");
     }
 
     @Test
-    @DisplayName("이름 조건 검색 테스트")
+    @DisplayName("전체 이름 조건 검색 테스트")
+    void fullNameConditionSelectTest() throws ImproperlyConfigured {
+        assertEquals(1, db.select(new NameCompareOption("TESTER CHOI")).size(), "검색 결과 존재하는 케이스");
+        assertEquals(0, db.select(new NameCompareOption("TAK DOKGO")).size(), "검색 결과 존재하지 않는 케이스");
+    }
+
+    @Test
+    @DisplayName("이름(성) 조건 검색 테스트")
+    void lastNameConditionSelectTest() throws ImproperlyConfigured {
+        assertEquals(1, db.select(new LastNameCompareOption("CHOI")).size(), "검색 결과 존재하는 케이스");
+        assertEquals(0, db.select(new LastNameCompareOption("DOKGO")).size(), "검색 결과 존재하지 않는 케이스");
+    }
+
+    @Test
+    @DisplayName("이름(Firstname) 조건 검색 테스트")
     void firstNameConditionSelectTest() throws ImproperlyConfigured {
-        assertEquals(1, db.select(new FirstNameCompareOption("TESTER")).size(), "Select 결과 확인");
+        assertEquals(1, db.select(new FirstNameCompareOption("TESTER")).size(), "검색 결과 존재하는 케이스");
+        assertEquals(0, db.select(new FirstNameCompareOption("NAMENOTEXIST")).size(), "검색 결과 존재하지 않는 케이스");
+    }
+
+    @Test
+    @DisplayName("경력개발단계 조건 검색 테스트")
+    void carrierLevelConditionSelectTest() throws ImproperlyConfigured {
+        assertEquals(2, db.select(new CareerLevelCompareOption("CL1")).size(), "검색 결과 존재하는 케이스");
+        assertEquals(0, db.select(new CareerLevelCompareOption("CL6")).size(), "검색 결과 존재하지 않는 케이스");
     }
 
     @Test
     @DisplayName("전화번호 중간자리 검색 테스트")
     void middlePhoneNumberSelectTest() throws ImproperlyConfigured {
-        assertEquals(1, db.select(new MiddlePhoneNumberCompareOption("9874")).size(), "전화번호 중간자리 검색 결과 확인");
+        assertEquals(1, db.select(new MiddlePhoneNumberCompareOption("9874")).size(), "검색 결과 존재하는 케이스");
+        assertEquals(0, db.select(new MiddlePhoneNumberCompareOption("9898")).size(), "검색 결과 존재하지 않는 케이스");
     }
 
     @Test
     @DisplayName("전화번호 끝자리 검색 테스트")
     void lastPhoneNumberSelectTest() throws ImproperlyConfigured {
-        assertEquals(1, db.select(new LastPhoneNumberCompareOption("2222")).size(), "전화번호 끝자리 검색 결과 확인");
+        assertEquals(1, db.select(new LastPhoneNumberCompareOption("2222")).size(), "검색 결과 존재하는 케이스");
+        assertEquals(0, db.select(new LastPhoneNumberCompareOption("4937")).size(), "검색 결과 존재하지 않는 케이스");
+    }
+
+    @Test
+    @DisplayName("생일(전체) 검색 테스트")
+    void birthdaySelectTest() throws ImproperlyConfigured {
+        assertEquals(1, db.select(new BirthdayCompareOption("19661010")).size(), "검색 결과 존재하는 케이스");
+        assertEquals(0, db.select(new BirthdayCompareOption("20221231")).size(), "검색 결과 존재하지 않는 케이스");
+    }
+
+    @Test
+    @DisplayName("생일(연도) 검색 테스트")
+    void birthdayYearSelectTest() throws ImproperlyConfigured {
+        assertEquals(1, db.select(new BirthdayYearCompareOption("1966")).size(), "검색 결과 존재하는 케이스");
+        assertEquals(0, db.select(new BirthdayYearCompareOption("2022")).size(), "검색 결과 존재하지 않는 케이스");
+    }
+
+    @Test
+    @DisplayName("생일(월) 검색 테스트")
+    void birthdayMonthSelectTest() throws ImproperlyConfigured {
+        assertEquals(1, db.select(new BirthdayMonthCompareOption("10")).size(), "검색 결과 존재하는 케이스");
+        assertEquals(0, db.select(new BirthdayMonthCompareOption("12")).size(), "검색 결과 존재하지 않는 케이스");
+    }
+
+    @Test
+    @DisplayName("생일(일) 검색 테스트")
+    void birthdayDaySelectTest() throws ImproperlyConfigured {
+        assertEquals(1, db.select(new BirthdayDayCompareOption("06")).size(), "검색 결과 존재하는 케이스");
+        assertEquals(0, db.select(new BirthdayDayCompareOption("31")).size(), "검색 결과 존재하지 않는 케이스");
+    }
+
+    @Test
+    @DisplayName("CERTI 검색 테스트")
+    void certiSelectTest() throws ImproperlyConfigured {
+        assertEquals(1, db.select(new CertificationCompareOption("PRO")).size(), "검색 결과 존재하는 케이스");
+        assertEquals(0, db.select(new CertificationCompareOption("IM")).size(), "검색 결과 존재하지 않는 케이스");
     }
 }
