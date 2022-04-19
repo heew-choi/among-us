032fbe0128d2754ca28c153c36e2af7f1ea74516
BronnzSon <79143217+BronnzSon@users.noreply.github.com>
2022-04-19 14:35:49 +0900

    [Fix] AddCommand 임시변수 제거

--- src/main/java/command/AddCommand.java
+++ src/main/java/command/AddCommand.java
@@ -2,8 +2,6 @@ package command;
 
 import database.Database;
 import employee.Employee;
-import exceptions.ImproperlyConfigured;
-import option.compare.EmployeeNumberCompareOption;
 
 import java.util.List;
 
@@ -27,12 +25,6 @@ public class AddCommand extends Command {
     }
 
     public Employee makeNewbie(List<String> params) {
-        String empNum = params.get(0);
-        String name = params.get(1);
-        String cl = params.get(2);
-        String phoneNum = params.get(3);
-        String birthday = params.get(4);
-        String certi = params.get(5);
-        return new Employee(empNum, name, cl, phoneNum, birthday, certi);
+        return new Employee(params.get(0), params.get(1), params.get(2), params.get(3), params.get(4), params.get(5));
     }
 }
